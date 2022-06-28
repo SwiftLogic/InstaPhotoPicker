1d2963f9318d2ea00124b51220d7db3bc4ab8d4a
Samuel Uyigue <Samuel.uyigue@gmail.com>
2022-06-28 17:04:11 -0500

    Initial Commit

---
+++ InstaPhotoPicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaPhotoPicker
+//
+//  Created by Osaretin Uyigue on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
